21461c0dc683c35a2ed2e8d8143ff4916cb44a60
Shadow181013 <101969915+Shadow181013@users.noreply.github.com>
2024-05-08 16:00:14 +0800

    new user

--- FirstClass21Team.java
+++ FirstClass21Team.java
@@ -4,5 +4,6 @@ public class FirstClass21Team
     {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157010\"\n姓名：\"蔡豐蔚\"");
+        System.out.println("\"學號：\"01157024\"\n姓名：\"蕭宇翔\"");
     }
 }
