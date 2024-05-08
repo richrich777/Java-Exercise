53e92f7bb7e085f9f31f15b08b66c7fd04f1dd77
richrich777 <145979824+richrich777@users.noreply.github.com>
2024-05-08 15:57:12 +0800

    蔡豐蔚
    member1

---
+++ FirstClass21Team.java
@@ -0,0 +1,8 @@
+public class FirstClass21Team
+{
+    public static void main(String[] args)
+    {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01157010\"\n姓名：\"蔡豐蔚\"");
+    }
+}
